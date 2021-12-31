c4a3da6970253f9ca8aadd4bebc8b1db19324d9b
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2021-12-30 23:51:43 -0500

    janky fixed a css bug where the exit button of the logtail bleeds off the page, and fixed the positioning of the navbar items

--- src/components/LogtailComponent.tsx
+++ src/components/LogtailComponent.tsx
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 
 
 const LogtailComponentWrapper = styled.section<StyledLogtailType>`
-width: 45vw;
+
 background-color: #26374C;
 margin: 10px;
 overflow: hidden;
 resize: horizontal;
-max-width: ${(props) => props.templateIsVisible ? '70vw' : '100vw'};
-min-width: ${(props) => props.templateIsVisible ? '30vw' : '100vw'};
+max-width: ${(props) => props.templateIsVisible ? '70vw' : '97vw'};
+min-width: ${(props) => props.templateIsVisible ? '30vw' : '97vw'};
 height: ${(props) => props.wordCloudIsVisible ? '65vh' : '100vw'};
 width: ${(props) => props.templateIsVisible ? '45vw' : '90vw'};
 overflow: auto;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Button } from 'stories/Button'
 import styled from 'styled-components'
 
 
 
+const StyledNavWrapper = styled.nav`
+display: flex;
+position: fixed;
+height: 100px;
+width: 100%;
+opacity: 0.9;   
+`
+
+
 const StyledNav = styled.nav<StyledNavType>`
-    position: fixed;
-    height: 15vh;
-    width: 100%;
-    opacity: 0.9;   
+
+  
+    
 `
 
 const UnhideComponentItem = styled.span<StyledNavType>`
@@ -17,22 +26,66 @@ padding-left: 1rem;
 `
 
 const UnhideComponentWrapper = styled.div`
-position: absolute;
+height: 50px;
 display: flex;
+position: relative;
 top: 0;
-right: 2rem;
+right: 0;
 `
+
+const StyledInput = styled.input`
+
+    width: 90vw;
+    margin: 3px;
+    height: 2rem;
+    border: none;
+    border-radius: 10px;
+`
+
+
+
+const MenuButtonWrapper = styled.button`
+
+    width: 7rem;
+    height: 100%;
+    background: #57066F;
+    color: white;
+    border: none;
+    cursor: pointer;
+`
+
+const RadioButtonGroup = styled.span`
+    display: flex;
+`
+
+const ContentWrapper = styled.div`
+display: flex;
+justify-content: space-between;
+`
+
 
 export default function NavBar(props: INavBarProps) {
     //if logtail is not visible (or any other component), render the show button with a function in a higher order component
     return (
+        <StyledNavWrapper>
         <StyledNav>
+            <ContentWrapper>
+        <RadioButtonGroup>
+                <input type="radio"  value="Templates" /> Template
+                <input type="radio"  value="Variables"/> Variables
+                <input type="radio"  value="Both"/> Both
+            </RadioButtonGroup>
             <UnhideComponentWrapper>
             {(!props.logtailIsVisible && <UnhideComponentItem onClick={()=>{props.showComponent('logtailComponent')}}>X Show Logtail</UnhideComponentItem>)}
             {(!props.templateIsVisible && <UnhideComponentItem onClick={()=>{props.showComponent('templateTable')}}> X Show Template Table</UnhideComponentItem>)}
             {(!props.wordCloudIsVisible && <UnhideComponentItem onClick={()=>{props.showComponent('wordCloud')}}>X Show Word Cloud</UnhideComponentItem>)}
             </UnhideComponentWrapper>
+            </ContentWrapper>
+            <StyledInput type="text" placeholder="Search" />
+            
         </StyledNav>
+        <MenuButtonWrapper> menu goes here</MenuButtonWrapper>
+        </StyledNavWrapper>
     )
 }
 
