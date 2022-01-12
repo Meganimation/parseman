eda4c12395fbf6706e881285459096c368a42154
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2022-01-11 21:52:53 -0500

    adding logtail temp data and stylign

--- src/components/LogtailComponent.tsx
+++ src/components/LogtailComponent.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import styled from 'styled-components'
 
 //background-color:${(props) => props.darkMode ? '#26374C; ' : 'white'};
@@ -7,21 +7,47 @@ overflow: hidden;
 resize: horizontal;
 max-width: ${(props) => props.templateIsVisible ? '60vw' : '95vw'};
 min-width: ${(props) => props.templateIsVisible ? '30vw' : '95vw'};
-height: ${(props) => props.wordCloudIsVisible ? '43vh' : '35vw'};
+height: ${(props) => props.wordCloudIsVisible ? '49vh' : '35vw'};
 width: ${(props) => props.templateIsVisible ? '45vw' : '90vw'};
 overflow: auto;
-padding: 20px;
+
 
 background-color:${(props) => props.darkMode ? '#1C2937; ' : 'white'};
 border-radius: 10px;
 ` 
 
+const LogtailItem = styled.div<StyledLogtailType>`
+background: ${(props) => (props.id % 2 === 0) ? 'gray' : 'darkGray'};
+
+`
+
+
 
 export default function LogtailComponent(props: LogtailComponentProps) {
+
+    const inputEl = useRef(null)
+
+    const tempData = [
+        {id: 1, result: '2809-2733097930409-3-02- 2-38832- 2-83382- 2-80230-20932 3-2-293939 32-80230-20932 3- 2-2939393 2-80230-20932 3 -2-29393932-80230-20932 3-2-2939393'},
+        {id: 2, result: '28932 3 -2-29393932- 09-273309 7930409-3-02- 2-38832- 2-83382- 2-80230-20932 3-2-293939 32-80230-20932 3- 2-2939393 2-80230-2 80230-20932 3-2-2939393'},
+        {id: 3, result: '2809-2733097930409-3-02- 2-38832- 2-83382- 2-80230-20932 3-2-293939 32-80230-20932 3- 2-2939393 2-80230-20932 3 -2-29393932-80230-20932 3-2-2939393'},
+        {id: 4, result: '28932 3 -2-29393932- 09-273309 7930409-3-02- 2-38832- 2-83382- 2-80230-20932 3-2-293939 32-80230-20932 3- 2-2939393 2-80230-2 80230-20932 3-2-2939393'},
+    ]
+    
+    const mapData = (data: any) => {
+        return data.map((item: any) => {
+            return (<LogtailItem id={item.id} ><code>{item.result} {item.id}</code></LogtailItem>)
+        })
+    }
+
+   
+
+
+
     return (
         <LogtailComponentWrapper templateIsVisible={props.templateIsVisible} wordCloudIsVisible={props.wordCloudIsVisible} darkMode={props.darkMode} >
-            This is the LogtailComponent 
-            This is the LogtailComponent  This is the LogtailComponent  This is the LogtailComponent  This is the LogtailComponent  This is the LogtailComponent  This is the LogtailComponent
+
+         {mapData(tempData)}
         </LogtailComponentWrapper>
     )
 }
@@ -29,11 +55,13 @@ export default function LogtailComponent(props: LogtailComponentProps) {
 type StyledLogtailType = {
     templateIsVisible?: boolean;
     wordCloudIsVisible?: boolean;
-    darkMode: boolean;
+    darkMode?: boolean;
+    id?: any;
   };
 
 interface LogtailComponentProps {
     templateIsVisible: boolean;
     wordCloudIsVisible: boolean;
     darkMode: boolean;
+
   }
