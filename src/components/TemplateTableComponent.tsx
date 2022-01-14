01e866b0ad56f5c6932b4184e0c29f62707aa590
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2022-01-13 22:30:51 -0500

    fixed templatetable styling

--- src/components/TemplateTableComponent.tsx
+++ src/components/TemplateTableComponent.tsx
@@ -6,14 +6,34 @@ const TemplateTableComponentWrapper = styled.section<StyledTemplateType>`
   border-radius: 10px;
   overflow: auto;
   height: ${(props) => (props.wordCloudIsVisible ? "49vh" : "35vw")};
+
+  font-size: 12px;
+  border-top: 10px solid #1c2937;
 `;
+
+const TableHeaderWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+
+`;
+
 
 const TableWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 4fr 1fr;
-`;
+  background: #34404E;
+  cursor: pointer;
+  margin: 0;
+  border: 0;
+  align-items: center;
 
+  &:hover {
+    background: #28313B;
+  }
+
+`;
 const TableHeader = styled.h2`
+  padding-left: 10px;
   font-size: 0.75em;
   background: #2d4460;
   align-items: center;
@@ -27,9 +47,9 @@ const TableHeader = styled.h2`
   }
 `;
 
-const TableContent = styled.div`
-  border-top: 1px solid #2d4460;
-  margin: 3px;
+
+const StyledRadio = styled.input`
+  margin-right: 10px;
 `;
 
 const testData = [
@@ -41,7 +61,7 @@ const testData = [
   {
     templateId: 12311433,
     templateLiteral:
-      "bedue 2823b3 did290 2k3o3 dkdososb290 2k3o3 dkdososb290 2k3o3 dkdososb290 2k3o3 dkdososbdd e",
+      "bedue 2823b3 did290 2k3o3 dkdososb290 2k3o3 dkdososb290 2k3o3 dkdososb290 2 dkdososb290 2k3o3 dkdososb290 2k3o3 dkdososb k3o3 dkdososbdd e",
     totalLogs: 112,
   },
 ];
@@ -57,44 +77,31 @@ export default function TemplateTableComponent(
         wordCloudIsVisible={props.wordCloudIsVisible}
         darkMode={props.darkMode}
       >
-        <TableWrapper>
-          <div>
-            <TableHeader>
-              <h2>Template Id</h2>
-            </TableHeader>
+        <TableHeaderWrapper>
+          <TableHeader>
+            <h2>Template Id</h2>
+          </TableHeader>
 
+          <TableHeader>
+            <h2>Template Literal</h2>
+          </TableHeader>
+
+          <TableHeader>
+            <h2>Total Logs</h2>
+          </TableHeader>
+        </TableHeaderWrapper>
+
+          <div>
             {testData.map((data) => (
-              <TableContent>
-                <input type="radio" />
-                <b>{data.templateId}</b>
-              </TableContent>
+              <TableWrapper>
+               
+                <div> <StyledRadio type="radio" />{data.templateId}</div>
+                <div>{data.templateLiteral}</div>
+                <div>{data.totalLogs}</div>
+              </TableWrapper>
             ))}
           </div>
 
-          <div>
-            <TableHeader>
-              <h2>Template Literal</h2>
-            </TableHeader>
-
-            {testData.map((data) => (
-              <TableContent>
-                <code>{data.templateLiteral}</code>
-              </TableContent>
-            ))}
-          </div>
-
-          <div>
-            <TableHeader>
-              <h2>Total Logs</h2>
-            </TableHeader>
-
-            {testData.map((data) => (
-              <TableContent>
-                <i>{data.totalLogs}</i>
-              </TableContent>
-            ))}
-          </div>
-        </TableWrapper>
         <p>{testString}</p>
       </TemplateTableComponentWrapper>
     </>
--- src/components/WordCloudComponent.tsx
+++ src/components/WordCloudComponent.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const WordCloudComponentWrapper = styled.section<StyledWordCloudType>`
-height: 15vh;
+height: 9vh;
 background: ${(props) => props.darkMode ? '#26374b' : 'white'};
 margin: 10px;
 resize: horizontal;
@@ -19,20 +19,55 @@ overflow: auto;
 ` 
 
 const Text = styled.div`
+padding: 10px;
 
 `
+
+const WordsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    overflow: hidden;
+    width: 100%;
+
+
+    overflow: auto;
+
+`
+
+const tempData = [
+    {word: 'test', count: 1},
+    {word: 'test2', count: 2},
+    {word: 'test3', count: 3},
+    {word: 'test4', count: 4},
+    {word: 'test5', count: 5},
+    {word: 'test6', count: 6},
+    {word: 'test7', count: 7},
+    {word: 'test8', count: 8},
+    {word: 'test9', count: 9},
+    {word: 'test10', count: 10},
+    {word: 'test11', count: 11},
+    {word: 'test12', count: 12},
+]
+
 export default function WordCloudComponent(props: IWordCloudComponentProps) {
+
+
+    const mapWords = (tempData: any[]) => {
+        return tempData.map((word: any) => {
+            return <Text>{word.word}</Text>
+        })
+    }
+
     return (
         <>
         <WordCloudComponentWrapper darkMode={props.darkMode}>
-            <Text>
-            This is the WordCloudComponent
-            This is the WordCloudComponent
-            This is the WordCloudComponent
-            This is the WordCloudComponent
-            This is the WordCloudComponent
-            This is the WordCloudComponent
-            </Text>
+            <WordsContainer>
+            {mapWords(tempData)}
+            </WordsContainer>
         </WordCloudComponentWrapper>
         </>
 
