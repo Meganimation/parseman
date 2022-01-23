55ae3ff528edc3a177bb860225e7279de363f394
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2022-01-23 18:44:55 -0500

    new address attempt

--- src/utils/SelectorsHelper.tsx
+++ src/utils/SelectorsHelper.tsx
@@ -10,13 +10,13 @@ export const CURRENT_ENVIRONMENT_TYPE: string = EnvironmentTypeEnum.REMOTE;
 export default class SelectorsHelper {
   static getURL(environmentType: string, service: String) {
     console.log('envType', environmentType)
-    let retVal: string = `http://10.12.2.94:8081/${service}`;
+    let retVal: string = `http://10.0.10.30:8081/${service}`;
     switch (environmentType) {
       case EnvironmentTypeEnum.LOCAL:
         retVal = `http://localhost:8081/${service}`;
         break;
       case EnvironmentTypeEnum.REMOTE:
-        retVal = `http://10.12.2.249:8081/${service}`;
+        retVal = `http://10.0.10.30:8081/${service}`;
         break;
     }
     return retVal;
