eea4bb092e9a1dd01acb09599974e1ca69dc04c1
Mikhail Sigachev <47951044+sigachev@users.noreply.github.com>
2022-01-24 01:38:41 -0500

    Update SelectorsHelper.tsx
    aws endpoint server address changed

--- src/utils/SelectorsHelper.tsx
+++ src/utils/SelectorsHelper.tsx
@@ -16,7 +16,7 @@ export default class SelectorsHelper {
         retVal = `http://localhost:8081/${service}`;
         break;
       case EnvironmentTypeEnum.REMOTE:
-        retVal = `http://10.12.2.249:8081/${service}`;
+        retVal = `http://10.0.10.93:8082/${service}`;
         break;
     }
     return retVal;
