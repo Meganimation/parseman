12ac47c9595c005ae34f31f9855720d157df4e2c
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2022-03-02 08:45:26 -0500

    dynamic url

--- src/utils/SelectorsHelper.tsx
+++ src/utils/SelectorsHelper.tsx
@@ -2,6 +2,7 @@
 export const EnvironmentTypeEnum: any = {
   REMOTE: process.env.REACT_APP_REMOTE, 
   AWS:  process.env.REACT_APP_AWS,
+  URL: process.env.REACT_APP_URL
 }
 
 
@@ -18,26 +19,28 @@ console.log(process.env.REACT_APP_SERVER)
 
 
 export default class SelectorsHelper {
-  static getURL(environmentType: string, service: String) {
+  static getURL(environmentType: string, service: string) {
     console.log('envType', environmentType)
-    let retVal: string = `http://10.12.2.94:8081/${service}`;
+    let retVal: string = process.env.REACT_APP_URL + service
 
-    switch (process.env.REACT_APP_SERVER) {
-        case "aws":
-        retVal = `http://clickhouseapi.aws.sliceup.co:8082/${service}`;
-        break;
-        case "kubedemo":
-        case EnvironmentTypeEnum.REMOTE:
-        retVal = `http://clickhouseapi.kubedemo.sliceup.co/${service}`
-        break;
-        case "remote":
-              retVal = `http://10.12.2.242:8081/${service}`
-              break;
-    }
+
+    // switch (process.env.REACT_APP_SERVER) {
+    //     case "aws":
+    //     retVal = `http://clickhouseapi.aws.sliceup.co:8082/${service}`;
+    //     break;
+    //     case "kubedemo":
+    //     case EnvironmentTypeEnum.REMOTE:
+    //     retVal = `http://clickhouseapi.kubedemo.sliceup.co/${service}`
+    //     break;
+    //     case "remote":
+    //           retVal = `http://10.12.2.242:8081/${service}`
+    //           break;
     return retVal;
+    }
+
   }
 
-}
+
 
 
 
