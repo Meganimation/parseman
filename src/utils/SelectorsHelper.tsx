44fc9af6480c30b7551faa9055c5eea858218937
Mikhail Sigachev <47951044+sigachev@users.noreply.github.com>
2022-03-09 12:07:39 -0500

    Update SelectorsHelper.tsx

--- src/utils/SelectorsHelper.tsx
+++ src/utils/SelectorsHelper.tsx
@@ -1,36 +1,16 @@
-
-export enum EnvironmentTypeEnum {
-  OFFLINE = "offline",
-  LOCAL = "local",
-  REMOTE = "remote",
-  AWS = "aws",
+export const EnvironmentTypeEnum: any = {
+  AWS:  process.env.REACT_APP_AWS,
+  URL: process.env.REACT_APP_URL
 }
 
-export const CURRENT_ENVIRONMENT_TYPE: string = EnvironmentTypeEnum.AWS;
+//@ts-ignore
+export let CURRENT_ENVIRONMENT_TYPE: string = process.env.REACT_APP_SERVER
 
 export default class SelectorsHelper {
-  static getURL(environmentType: string, service: String) {
-    console.log('envType', environmentType)
-    let retVal: string = `http://10.12.2.94:8081/${service}`;
-    switch (environmentType) {
-      case EnvironmentTypeEnum.LOCAL:
-        retVal = `http://localhost:8081/${service}`;
-        break;
-        case EnvironmentTypeEnum.AWS:
-        retVal = `http://clickhouseapi.aws.sliceup.co:8082/${service}`;
-        break;
-        case EnvironmentTypeEnum.REMOTE:
-        retVal = `http://clickhouseapi.kubedemo.sliceup.co/${service}`
-        break;
-
-            // case EnvironmentTypeEnum.REMOTE:
-            //   retVal = `http://10.12.2.242:8081/${service}`
-            //   break;
-    }
+  static getURL(environmentType: string, service: string) {
+    let retVal: string = process.env.REACT_APP_URL + service
     return retVal;
+    }
   }
-
-}
-
 
 
